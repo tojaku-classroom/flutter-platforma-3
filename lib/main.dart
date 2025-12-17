f05e07bdee07c5e7cd5e57c817ad66fdd768c001
Tomislav Jakupić <tojaku@gmail.com>
2025-12-17 08:32:45 +0100

    Gotov Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -26,12 +26,20 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _controller = TextEditingController();
+  String _name = '';
 
   void showMessage(BuildContext context) {
+    String message = '';
+    if (_controller.text.isEmpty) {
+      message = 'Molimo unesite ime';
+    } else {
+      message = 'Pozdrav ${_controller.text}';
+    }
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text('Pozdrav ${_controller.text}'),
-        duration: Duration(seconds: 5),
+        content: Text(message),
+        duration: Duration(seconds: 2),
         behavior: SnackBarBehavior.fixed,
       ),
     );
@@ -42,9 +50,29 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text('Moja prva Flutter aplikacija')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => showMessage(context),
-          child: const Text('Pritisni me'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: 'Upišite svoje ime',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _name = value;
+                });
+              },
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () => showMessage(context),
+              child: const Text('Pritisni me'),
+            ),
+            const SizedBox(height: 24),
+            Text('Upisali ste $_name'),
+          ],
         ),
       ),
     );
